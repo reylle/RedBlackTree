1c7915068da475c676aba575d6ddb12bcadf8891
João Felipe Humenhuk <joaofh12@gmail.com>
2019-09-27 18:06:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import rbt_object
 
+from colorama import init, Fore, Style
 
-def print_tree_graphic(nodes, edges):
+
+def print_tree_graphic(tree):
+    nodes, edges = tree.export()
     g = nx.DiGraph()
     for node in nodes:
         color = node.get_color().lower()
@@ -20,59 +23,69 @@ def print_tree_graphic(nodes, edges):
     plt.show()
 
 
-def print_tree_console(nodes, edges):
+def print_tree_console(tree):
+    nodes, edges = tree.export()
     key = nodes[0].get_key()
-    print(key)
+    if nodes[0].get_color() == 'RED':
+        print(Fore.RED + str(key) + Style.RESET_ALL)
+    else:
+        print(str(key))
 
     cur_level = [key]
     next_level = []
-    line = ''
 
     for edge in edges:
         if edge[0] in cur_level:
-            line += str(edge[1]) + '\t'
+            if edge[1] != -1:
+                if tree.search(edge[1]).get_color() == 'RED':
+                    print(Fore.RED + str(edge[1]) + Style.RESET_ALL, end='\t')
+                else:
+                    print(str(edge[1]), end='\t')
+            else:
+                print(str(edge[1]), end='\t')
             next_level += [edge[1]]
         else:
-            print(line)
-            line = str(edge[1]) + '\t'
+            print('\n', end='')
+            if edge[1] != -1:
+                if tree.search(edge[1]).get_color() == 'RED':
+                    print(Fore.RED + str(edge[1]) + Style.RESET_ALL, end='\t')
+                else:
+                    print(str(edge[1]), end='\t')
+            else:
+                print(str(edge[1]), end='\t')
             cur_level = next_level
             next_level = [edge[1]]
-    print(line)
     print('\n')
 
 
 def main():
-
+    init(convert=True)
     tree = rbt_object.RedBlackTree(12)
 
-    nodes, edges = tree.export()
-    # print_tree_graphic(nodes, edges)
-    print_tree_console(nodes, edges)
+    # print_tree_graphic(tree)
+    print_tree_console(tree)
 
     tree.insert_node(7)
     tree.insert_node(6)
     tree.insert_node(5)
     tree.insert_node(15)
 
-    nodes, edges = tree.export()
-    # print_tree_graphic(nodes, edges)
-    print_tree_console(nodes, edges)
+    # print_tree_graphic(tree)
+    print_tree_console(tree)
 
     tree.delete_node(tree.search(5))
     tree.delete_node(tree.search(12))
 
-    nodes, edges = tree.export()
-    # print_tree_graphic(nodes, edges)
-    print_tree_console(nodes, edges)
+    # print_tree_graphic(tree)
+    print_tree_console(tree)
 
     tree.insert_node(23)
     tree.insert_node(27)
     tree.insert_node(32)
     tree.insert_node(2)
 
-    nodes, edges = tree.export()
-    # print_tree_graphic(nodes, edges)
-    print_tree_console(nodes, edges)
+    # print_tree_graphic(tree)
+    print_tree_console(tree)
 
     print('Complete :3')
 
